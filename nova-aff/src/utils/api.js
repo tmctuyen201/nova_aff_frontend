7b08d14dd6c1cd634dc81aef5e15751daa699627
Admin <admin@vinasupport.com>
2025-08-27 13:22:18 +0700

    5

--- nova-aff/src/utils/api.js
+++ nova-aff/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "https://novaaff.id.vn/api";
 
 // Helper function to make API requests
 const apiRequest = async (endpoint, options = {}) => {
